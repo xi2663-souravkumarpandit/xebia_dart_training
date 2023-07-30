968d916d905fd6a7a45a3f069f8dbe8edf28b8be
Sourav Pandit <87409366+xi2663-souravkumarpandit@users.noreply.github.com>
2023-07-30 17:08:05 +0530

    first commit

---
+++ bin/xebia_dart_training.dart
@@ -0,0 +1,5 @@
+import 'package:xebia_dart_training/xebia_dart_training.dart' as xebia_dart_training;
+
+void main(List<String> arguments) {
+  print('Hello world: ${xebia_dart_training.calculate()}!');
+}
---
+++ test/xebia_dart_training_test.dart
@@ -0,0 +1,8 @@
+import 'package:xebia_dart_training/xebia_dart_training.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
